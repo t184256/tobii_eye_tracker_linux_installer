609c418f85466c2c4920906100472ca0327afde6
Alexander Sosedkin <monk@unboiled.info>
2021-03-13 22:16:56 +0100

    flake.nix: minor cleanup

--- flake.nix
+++ flake.nix
@@ -6,9 +6,6 @@
   };
 
   outputs = { self, nixpkgs }: {
-    #packages.x86_64-linux.hello = nixpkgs.legacyPackages.x86_64-linux.hello;
-    #defaultPackage.x86_64-linux = self.packages.x86_64-linux.hello;
-
     packages.x86_64-linux = with import nixpkgs { system = "x86_64-linux"; };
     rec {
       tobii-usbservice = stdenv.mkDerivation {
